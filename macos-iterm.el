1a83ee00a3057148ce3717590eb80115d33b1488
Jacob Chaffin <jchaffin@ucla.edu>
2018-07-23 13:06:06 -0700

    Fix typo in command map.

--- macos-iterm.el
+++ macos-iterm.el
@@ -74,10 +74,9 @@ With PREFIX, cd to project root."
        "end tell")
       cmd))))
 
-
 (defvar macos-iterm-command-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "g") #'iterm-goto-fildir-or-home)
+    (define-key map (kbd "g") #'iterm-goto-filedir-or-home)
     (define-key map (kbd "f") #'iterm-focus)
     (define-key map (kbd "s") #'iterm-shell-command)
     map)
